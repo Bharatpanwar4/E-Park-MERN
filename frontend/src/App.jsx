aa8122553f87051fbcb2733cfc6e4627c1f341fe
Bharatpanwar4 <111150558+Bharatpanwar4@users.noreply.github.com>
2023-05-31 23:06:11 +0530

    setup backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import Header from './components/Header'
 import { Container } from 'react-bootstrap'
-import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
 
+import Footer from './components/Footer'
 const App = () => {
   return (
     <>
@@ -11,8 +10,8 @@ const App = () => {
 <Header/>
 <main className='py-3'>
 <Container>
-<HomeScreen/>
 
+<Outlet/>
 </Container>
 </main>
 <Footer/>
