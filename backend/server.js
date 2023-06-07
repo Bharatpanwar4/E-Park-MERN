27eeb24fdf707ded9d226000b7073c3b42758adf
Bharatpanwar4 <111150558+Bharatpanwar4@users.noreply.github.com>
2023-06-08 03:21:07 +0530

    used express routers and dislplay data from backend to HomeScreen

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import  express  from "express";
 import dotenv from 'dotenv'
 dotenv.config()
 import products from "./data/products.js";
+import productRoutes from "./routes/productRoute.js"
 import connectDB from "./config/db.js";
 const port=process.env.PORT
 
@@ -13,15 +14,9 @@ app.get('/',(req,res)=>{
 
     res.send('Api is running')
 })
-app.get('/api/products',(req,res)=>{
 
-    res.json(products)
-})
+app.use('/api/products',productRoutes)
 
-app.get('/api/products/:id',(req,res)=>{
-const product=products.find((p)=>p._id===req.params.id)
-    res.json(product)
-})
 
 
 app.listen(port,()=>{console.log(`server is running on ${port}`);})
