041d8e680988cb379dd82a8a03b30e4cbb7cbc40
Bharatpanwar4 <111150558+Bharatpanwar4@users.noreply.github.com>
2023-06-10 23:13:38 +0530

    crete errorhandler file

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -8,6 +8,7 @@ const router =express.Router()
 
 router.get('/',asyncHandler(async(req,res)=>{
 const products = await Product.find({})
+throw new Error ('Some Error')
     res.json(products)
 }));
 
@@ -15,7 +16,11 @@ router.get('/:id',asyncHandler(async(req,res)=>{
     const product = await Product.findById(req.params.id)
     if(product)
     {return res.json(product)}
-    res.status(404).json({message : `Product not found`})
+    else{
+        res.status(404)
+        throw new Error('Resource not found')
+    }
+    
 }));
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import  express  from "express";
 import dotenv from 'dotenv'
 dotenv.config()
-import products from "./data/products.js";
 import productRoutes from "./routes/productRoute.js"
 import connectDB from "./config/db.js";
+import { notFound,errorHandler } from "./middleware/errorMiddleware.js";
 const port=process.env.PORT
 
 
@@ -17,6 +17,7 @@ app.get('/',(req,res)=>{
 
 app.use('/api/products',productRoutes)
 
-
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port,()=>{console.log(`server is running on ${port}`);})
